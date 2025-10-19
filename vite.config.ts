395974ec051f7caac1da62af60593deba756cca2
Immanuel Capistrano <icapistrano97@outlook.com>
2025-10-20 00:03:59 +0100

    Fix public path

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   base: "/valentine/",
-  build: {
-    outDir: "dist/valentine", // Build output folder
-  },
   plugins: [react(), gltf()],
 }));
